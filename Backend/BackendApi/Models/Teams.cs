9dfbb64e6ecff08c93c5350ec7c00e97511d7e9e
Deborah <deborahdev.io@gmail.com>
2025-01-02 11:42:19 +0100

    change todo route by team route

--- Backend/BackendApi/Models/Teams.cs
+++ Backend/BackendApi/Models/Teams.cs
@@ -1,7 +1,8 @@
 namespace BackendApi.Models;
 
-public class ToDo
+public class Teams
 {
     public int Id { get; set; }
     public string Name { get; set; }
+    public string? Image { get; set; }
 }
