21551ae95fb83a4d6bbdaea0ebbd0fb32f0565bb
Deborah <deborahdev.io@gmail.com>
2025-01-15 08:53:18 +0100

    wip dropdown

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,36 +1,46 @@
-import { Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component, viewChild} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {Subscription} from 'rxjs';
-import {TitleCasePipe} from '@angular/common';
+import {Observable, Subscription} from 'rxjs';
+import {AsyncPipe, NgClass, TitleCasePipe} from '@angular/common';
 import {TeamService} from './core/services/team.service';
 import {ITeam} from './core/models/team.model';
+import {MatIconModule} from '@angular/material/icon';
+import {NgFor} from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TitleCasePipe],
+  imports: [
+    MatIconModule,
+    RouterOutlet,
+    NgFor,
+    TitleCasePipe,
+    AsyncPipe,
+    NgClass,
+
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
 
-  public team : ITeam[] | undefined
-  public teamSubscription: Subscription | undefined
+  public teamList$?: Observable<ITeam[]>;
+  public active = false;
+  private _event: any;
 
-  constructor(private readonly teamService: TeamService) { }
+  constructor(private readonly teamService: TeamService) {}
 
   ngOnInit(): void {
-    this.getTeams()
+    this.teamList$ = this.teamService.getTeams();
+
   }
 
-  public getTeams():void{
-    this.teamSubscription = this.teamService
-      .getTeams()
-      .subscribe(
-        (_team) => {
-          this.team = _team;
-          console.log("team :", this.team);
-        });
+  public toggleSubMenu(event){
+    this._event = event;
+    console.log('test')
+    this.active = !this.active;
   }
 
 }
