0a5ff5a1ad5d2e834ef752107e9948076d67e591
Deborah <deborahdev.io@gmail.com>
2025-01-18 12:48:07 +0100

    set dropdown

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,24 +1,18 @@
-import {ChangeDetectionStrategy, Component, viewChild} from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 import {Observable, Subscription} from 'rxjs';
-import {AsyncPipe, NgClass, TitleCasePipe} from '@angular/common';
+import {AsyncPipe} from '@angular/common';
 import {TeamService} from './core/services/team.service';
 import {ITeam} from './core/models/team.model';
-import {MatIconModule} from '@angular/material/icon';
 import {NgFor} from '@angular/common';
-
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    MatIconModule,
     RouterOutlet,
     NgFor,
-    TitleCasePipe,
     AsyncPipe,
-    NgClass,
-
   ],
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './app.component.html',
@@ -28,19 +22,15 @@ export class AppComponent {
 
   public teamList$?: Observable<ITeam[]>;
   public active = false;
-  private _event: any;
+  public selectedTeam: string = 'Equipe';
 
   constructor(private readonly teamService: TeamService) {}
 
   ngOnInit(): void {
     this.teamList$ = this.teamService.getTeams();
-
   }
 
-  public toggleSubMenu(event){
-    this._event = event;
-    console.log('test')
-    this.active = !this.active;
+  selectTeam(teamName: string): void {
+    this.selectedTeam = teamName;
   }
-
 }
