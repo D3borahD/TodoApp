896388d6498f13c1529a59bf8be9ba903617b167
Deborah <deborahdev.io@gmail.com>
2025-02-02 19:57:52 +0100

    ajout de la navigation pour les projets en front - non fonctionnel

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { provideRouter } from '@angular/router';
+import {TeamsProjetsComponent} from './teamsProjets/teams-projets/teams-projets.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'teamsProjets', component: TeamsProjetsComponent}
+
+];
+
