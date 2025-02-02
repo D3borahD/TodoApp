29fabcdf8797e209cb467ed45025da96ff0cb00d
Deborah <deborahdev.io@gmail.com>
2025-02-02 19:57:52 +0100

    ajout de la navigation pour les projets en front - non fonctionnel

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,18 +1,22 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
-import {RouterOutlet} from '@angular/router';
-import {Observable, Subscription} from 'rxjs';
+import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
+import {Observable, of, Subscription} from 'rxjs';
 import {AsyncPipe} from '@angular/common';
 import {TeamService} from './core/services/team.service';
 import {ITeam} from './core/models/team.model';
 import {NgFor} from '@angular/common';
+import {TeamsProjetsComponent} from './teamsProjets/teams-projets/teams-projets.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
     NgFor,
     AsyncPipe,
+    TeamsProjetsComponent,
   ],
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './app.component.html',
@@ -20,7 +24,7 @@ import {NgFor} from '@angular/common';
 })
 export class AppComponent {
 
-  public teamList$?: Observable<ITeam[]>;
+  public teamList$!: Observable<ITeam[]>;
   public active = false;
   public selectedTeam: string = 'Equipe';
 
@@ -33,4 +37,6 @@ export class AppComponent {
   selectTeam(teamName: string): void {
     this.selectedTeam = teamName;
   }
+
+
 }
