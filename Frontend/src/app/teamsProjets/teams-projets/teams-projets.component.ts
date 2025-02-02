0263f4d6e4de60fae70239955453d5378e03fafa
Deborah <deborahdev.io@gmail.com>
2025-02-02 20:00:13 +0100

    ajout d'un component projet d'équipe

---
+++ Frontend/src/app/teamsProjets/teams-projets/teams-projets.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-teams-projets',
+  standalone: true,
+  imports: [],
+  templateUrl: './teams-projets.component.html',
+  styleUrl: './teams-projets.component.scss'
+})
+export class TeamsProjetsComponent {
+
+}
