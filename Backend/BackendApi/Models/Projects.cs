489f3c6164717968b9346ff62cb664931cd96fc5
Deborah <deborahdev.io@gmail.com>
2025-02-02 19:55:53 +0100

    ajout d'un CRUD projet et du modele projet en back

---
+++ Backend/BackendApi/Models/Projects.cs
@@ -0,0 +1,10 @@
+namespace BackendApi.Models;
+
+public class Projects
+{
+    public int Id { get; set; }
+    public string Reference { get; set; }
+    public string Name { get; set; }
+    public string Lot { get; set; }
+
+}
