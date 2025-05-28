b2e81d53f9ce46aa90e9737614d6be8927a7713f
Deborah <deborahdev.io@gmail.com>
2025-05-28 20:04:30 +0200

    operational getTask with signal method

--- Frontend/src/app/tasks/tasks.component.ts
+++ Frontend/src/app/tasks/tasks.component.ts
@@ -1,8 +1,10 @@
-import {Component, inject, signal} from '@angular/core';
+import {Component, inject, OnInit, signal} from '@angular/core';
 import {Task} from '../core/models/task.model';
 import {TaskService} from '../core/services/task.service';
 import {TaskListComponent} from '../component/task-list/task-list.component';
 import {NewElementComponent} from '../component/new-element/new-element.component';
+import {toSignal} from '@angular/core/rxjs-interop';
+import {Observable} from 'rxjs';
 
 
 @Component({
@@ -15,19 +17,28 @@ import {NewElementComponent} from '../component/new-element/new-element.componen
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.scss'
 })
-export class TasksComponent {
+export class TasksComponent implements OnInit {
   public title:string | null = 'Saisie des temps'
 
   taskService = inject(TaskService)
 
-  taskList= signal<Task[]>([])
+  //taskList= signal<Task[]>([])
+ // taskList: Task[]
+  taskListSignal
+  //taskList$: Observable<Task[]> = this.taskService.getTasks()
 
 
   constructor(
   ) {
-    console.log(this.taskService.getAllTasks())
+   // console.log(this.taskService.getAllTasks())
+  //  this.taskList= this.taskService.getAllTasks()
+    this.taskListSignal = toSignal(this.taskService.getTasks())
 
-    this.taskList.set(this.taskService.getAllTasks())
+  }
+
+  ngOnInit() {
+    //this.taskList = this.taskService.getAllTasks()
+
   }
 
 
