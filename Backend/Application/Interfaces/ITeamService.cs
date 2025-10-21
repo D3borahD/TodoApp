e1ade3ac56778381cc79f93526afae978d71239d
Deborah <deborahdev.io@gmail.com>
2025-10-21 12:08:50 +0200

    add created and getById team

--- Backend/Application/Interfaces/ITeamService.cs
+++ Backend/Application/Interfaces/ITeamService.cs
@@ -1,3 +1,4 @@
+using BackendApi.Models;
 using Domain.DTO;
 
 namespace Application.Interfaces;
@@ -5,4 +6,6 @@ namespace Application.Interfaces;
 public interface ITeamService
 {
     public Task<List<TeamDto>> GetTeamsAsync();
+    public Task<TeamDto?> GetTeamsByIdAsync(int id);
+    public Task<TeamDto?> CreateTeamAsync(TeamDto teamDto);
 }
--- Backend/Infrastructure/Repository/TeamRepository.cs
+++ Backend/Infrastructure/Repository/TeamRepository.cs
@@ -1,22 +1,36 @@
+using System.Text;
+using System.Text.Encodings.Web;
 using System.Text.Json;
 using BackendApi.Helpers;
 using BackendApi.Models;
+using Domain.DTO;
 using Infrastructure.IRepository;
+using Microsoft.Extensions.Logging;
 
 namespace Infrastructure.Repository;
 
 public class TeamRepository : ITeamRepository
 {
     private readonly string _path = FilePathHelper.GetPath("teamsDatas");
+    private readonly ILogger<TeamRepository> _logger;
+
+    public TeamRepository(ILogger<TeamRepository> logger)
+    {
+        _logger = logger;
+    }
     
+    public static readonly JsonSerializerOptions options = new JsonSerializerOptions
+    {
+        WriteIndented = true,
+        Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+    };
+
     public async Task<List<TeamDao>> GetTeamsAsync()
     {
         try
         {
             if (!File.Exists(_path))
-            {
-                return new List<TeamDao>();
-            }
+                _logger.LogInformation("No team found");
 
             string json = await File.ReadAllTextAsync(_path);
 
@@ -26,8 +40,52 @@ public class TeamRepository : ITeamRepository
         }
         catch (Exception e)
         {
+            _logger.LogError(e, "Error during team search");
             return new List<TeamDao>();
         }
+    }
+
+    public async Task<TeamDao?> GetTeamByIdAsync(int id)
+    {
+        try
+        {
+            var teams = await GetTeamsAsync();
+            var team = teams.FirstOrDefault(t => t.Id == id);
+
+            if (team == null)
+                _logger.LogInformation("No team found for id {id}", id);
+            return team;
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "Error while searching for the team  {Id}", id);
+            return null;
+        }
+    }
+
+    public async Task<TeamDao> CreateTeamAsync(TeamDao teamDto)
+    {
+        try
+        {
+            var teamsList = await GetTeamsAsync();
+            teamsList.Add(teamDto);
+
+            // Sérialiser les données en JSON
+            string updatedJson = JsonSerializer.Serialize(teamsList, options);
+            // Écrire le JSON avec encodage explicite UTF-8
+            await File.WriteAllTextAsync(_path, updatedJson, Encoding.UTF8);
+
+            _logger.LogInformation("✅ New team created: {Label} (ID {Id})", teamDto.Label, teamDto.Id);
+       
+            return teamDto;
+
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "Error during team creation");
+            return null;
+        }
+            
     }
 }
 
