a8519536103094d4e3544cd694cb01f43b32cf97
Deborah <deborahdev.io@gmail.com>
2025-10-22 11:46:22 +0200

    add update and delete team

--- Backend/Application/Interfaces/ITeamService.cs
+++ Backend/Application/Interfaces/ITeamService.cs
@@ -8,4 +8,6 @@ public interface ITeamService
     public Task<List<TeamDto>> GetTeamsAsync();
     public Task<TeamDto?> GetTeamsByIdAsync(int id);
     public Task<TeamDto?> CreateTeamAsync(TeamDto teamDto);
+    public Task<TeamDto> UpdateTeamAsync(TeamDto teamDto);
+    public Task<bool> DeleteTeamAsync(int id);
 }
