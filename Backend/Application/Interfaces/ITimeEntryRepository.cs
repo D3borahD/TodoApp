04cf9706e510e9a0c377c7f3355b809bd9452ebb
Deborah <deborahdev.io@gmail.com>
2025-10-31 10:44:36 +0100

    update timeEntry CRUD

--- Backend/Application/Interfaces/ITimeEntryRepository.cs
+++ Backend/Application/Interfaces/ITimeEntryRepository.cs
@@ -6,7 +6,9 @@ public interface ITimeEntryRepository
 {
     public Task<List<TimeEntryDao>> GetAllAsync();
     public Task<TimeEntryDao?> GetByIdAsync(int id);
+    public Task<IEnumerable<TimeEntryDao>> GetByDateAsync(DateTime date);
     public Task<TimeEntryDao> CreateAsync(TimeEntryDao entity);
     public Task<TimeEntryDao?> UpdateAsync(TimeEntryDao entity);
     public Task DeleteAsync(int id);
+   
 }
--- Backend/Application/Interfaces/ITimeEntryService.cs
+++ Backend/Application/Interfaces/ITimeEntryService.cs
@@ -5,8 +5,9 @@ namespace Application.Interfaces;
 public interface ITimeEntryService 
 {
     public Task<List<TimeEntryDto>> GetAllAsync();
-    /*public Task<TimeEntryDto?> GetByIdAsync(int id);*/
+    public Task<IEnumerable<TimeEntryDto>?> GetByDateAsync(DateTime date);
+    /*public Task<TimeEntryDto> GetByIdAsync(int id);*/
     public Task<TimeEntryDto> CreateAsync(TimeEntryCreateDto entity);
-    /*public Task<TimeEntryDto?> UpdateAsync(TimeEntryDto entity);
-    public Task<bool> DeleteAsync(int id);*/
+    public Task<TimeEntryDto?> UpdateAsync(TimeEntryCreateDto entity);
+    public Task<bool> DeleteAsync(int id);
 }
