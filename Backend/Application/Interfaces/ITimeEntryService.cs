7445619cff0fd24d72fbc27b5b90c8eac9db980c
Deborah <deborahdev.io@gmail.com>
2026-02-28 16:51:48 +0100

    BACK : add endpoint getEntryTimeByCurrentMonth

--- Backend/Application/Interfaces/ITimeEntryService.cs
+++ Backend/Application/Interfaces/ITimeEntryService.cs
@@ -5,9 +5,10 @@ namespace Application.Interfaces;
 public interface ITimeEntryService 
 {
     public Task<List<TimeEntryDto>> GetAllAsync();
-    public Task<IEnumerable<TimeEntryDto>?> GetByDateAsync(DateTime date);
+    public Task<IEnumerable<TimeEntryDto>?> GetByCurrentMonthAsync(DateTime date);
     /*public Task<TimeEntryDto> GetByIdAsync(int id);*/
     public Task<TimeEntryDto> CreateAsync(TimeEntryCreateDto entity);
     public Task<TimeEntryDto?> UpdateAsync(TimeEntryCreateDto entity);
     public Task<bool> DeleteAsync(int id);
+
 }
