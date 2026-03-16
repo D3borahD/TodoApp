3ae4ac46e1a086c0a36acc97881f3108b3d3c5cf
Deborah <deborahdev.io@gmail.com>
2026-03-16 10:44:04 +0100

    Back : update GetByDateRange

--- Backend/Application/Interfaces/ITimeEntryRepository.cs
+++ Backend/Application/Interfaces/ITimeEntryRepository.cs
@@ -6,7 +6,7 @@ public interface ITimeEntryRepository
 {
     public Task<List<TimeEntryDao>> GetAllAsync();
     public Task<TimeEntryDao?> GetByIdAsync(int id);
-    public Task<IEnumerable<TimeEntryDao>> GetByCurrentMonthAsync(DateTime date);
+    public Task<IEnumerable<TimeEntryDao>> GetByDateRangeAsync(DateTime startDate, DateTime endDate);
     public Task<TimeEntryDao> CreateAsync(TimeEntryDao entity);
     public Task<TimeEntryDao?> UpdateAsync(TimeEntryDao entity);
     public Task DeleteAsync(int id);
--- Backend/Application/Interfaces/ITimeEntryService.cs
+++ Backend/Application/Interfaces/ITimeEntryService.cs
@@ -5,8 +5,7 @@ namespace Application.Interfaces;
 public interface ITimeEntryService 
 {
     public Task<List<TimeEntryDto>> GetAllAsync();
-    public Task<IEnumerable<TimeEntryDto>?> GetByCurrentMonthAsync(DateTime date);
-    /*public Task<TimeEntryDto> GetByIdAsync(int id);*/
+    public Task<IEnumerable<TimeEntryDto>?> GetByDateRangeAsync(DateTime startDate, DateTime endDate);
     public Task<TimeEntryDto> CreateAsync(TimeEntryCreateDto entity);
     public Task<TimeEntryDto?> UpdateAsync(TimeEntryCreateDto entity);
     public Task<bool> DeleteAsync(int id);
